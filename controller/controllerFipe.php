99167a769a9590f0e57d4aa9eaf2c37e83aad599
FABIO BARBOSASA <fabiobarbosa@gmx.com>
2019-03-13 02:03:46 -0300

    api fipe

--- controller/controllerFipe.php
+++ controller/controllerFipe.php
@@ -2,16 +2,8 @@
 include ("../class/ClassFipe.php");
 $objFipe=new ClassFipe();
 $action=$_GET['action'];
-if(isset($_GET['brandId'])){
-    $brandId=$_GET['brandId'];
-}else{
-    $brandId=null;
-}
-if(isset($_GET['vehicleId'])){
-    $vehicleId=$_GET['vehicleId'];
-}else{
-    $vehicleId=null;
-}
+if(isset($_GET['brandId'])){$brandId=$_GET['brandId'];}else{$brandId=null;}
+if(isset($_GET['vehicleId'])){$vehicleId=$_GET['vehicleId'];}else{$vehicleId=null;}
 
 if($action=='brand'){
     $objFipe->setUrl("http://fipeapi.appspot.com/api/1/carros/marcas.json");
